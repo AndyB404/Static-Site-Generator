1512a4541a47248fb74573d4c8e37df72dc7375f
AndyB404 <abrice007@gmail.com>
2026-03-17 15:48:04 +0000

    feat: add markdown_to_html_node conversion

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from htmlnode import ParentNode, LeafNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -43,3 +46,59 @@ def block_to_block_type(mdtext):
             line_number += 1 
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    html_nodes = []
+    for node in text_nodes:
+        html_nodes.append(text_node_to_html_node(node))
+    return html_nodes
+
+def markdown_to_html_node(markdown):
+    children = []
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type is BlockType.HEADING:
+            marker = block.split(" ")[0]
+            level = len(marker)
+            header_tag = f"h{level}" 
+            header_text = block.split(" ", 1)[1]
+            children.append(ParentNode(header_tag, text_to_children(header_text)))
+        elif block_type is BlockType.PARAGRAPH:
+            paragraph_text = block.replace("\n", " ")
+            children.append(ParentNode("p", text_to_children(paragraph_text)))
+        elif block_type is BlockType.CODE:
+            code_text = block[4:-3]
+            converted = text_node_to_html_node(TextNode(code_text, TextType.TEXT))
+            code_node = ParentNode("code", [converted])
+            pre_node = ParentNode("pre", [code_node])
+            children.append(pre_node)
+        elif block_type is BlockType.UNORDERED_LIST:
+            lines = block.split("\n")
+            items = []
+            for line in lines:
+                text = line[2:]
+                items.append(ParentNode("li", text_to_children(text)))
+            children.append(ParentNode("ul", items))
+        elif block_type is BlockType.ORDERED_LIST:
+            lines = block.split("\n")
+            items = []
+            for line in lines:
+                text = line.split(". ", 1)[1]
+                items.append(ParentNode("li", text_to_children(text)))
+            children.append(ParentNode("ol", items))
+        elif block_type is BlockType.QUOTE:
+            lines = block.split("\n")
+            cleaned = []
+            for line in lines:
+                if line.startswith("> "):
+                    text = line[2:]
+                else:
+                    text = line[1:]
+                cleaned.append(text)
+            quote_text = " ".join(cleaned)
+            children.append(ParentNode("blockquote", text_to_children(quote_text)))
+    return ParentNode("div", children)
+
+
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -82,3 +82,86 @@ This is one block of text
         block = "I am a paragraph\nthis is my text"
         self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_heading(self):
+        md = """
+# My Heading
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><h1>My Heading</h1></div>")
+
+    def test_heading_level3(self):
+        md = """
+### My Heading
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><h3>My Heading</h3></div>")
+
+    def test_unordered_list(self):
+        md = """
+- item one
+- item two
+- item three
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><ul><li>item one</li><li>item two</li><li>item three</li></ul></div>")
+
+    def test_ordered_list(self):
+        md = """
+1. first
+2. second
+3. third
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><ol><li>first</li><li>second</li><li>third</li></ol></div>")
+
+    def test_quote(self):
+        md = """
+> This is a quote
+> that spans lines
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><blockquote>This is a quote that spans lines</blockquote></div>")
+
+    def test_mixed_blocks(self):
+        md = """
+# Title
+
+A paragraph here.
+
+- item one
+- item two
+"""
+        node = markdown_to_html_node(md)
+        self.assertEqual(node.to_html(), "<div><h1>Title</h1><p>A paragraph here.</p><ul><li>item one</li><li>item two</li></ul></div>")
